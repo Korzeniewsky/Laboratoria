6512fcc329bc6225d5d0acd90b9fcfdccf395436
Kamil Korzeniewski <90689600+Korzeniewsky@users.noreply.github.com>
2023-12-12 09:19:52 +0100

    lab7 ulepszone
    lab 7 ze wskaźnikami we funkcjach i zrobionym zadaniem 5

--- lab7.cpp
+++ lab7.cpp
@@ -1,16 +1,19 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
 #include <ctime>
 #include <vector>
 #include <algorithm>
+#include <cmath>
+#include <math.h>
 
-std::vector <int> zad_1(std::vector <int> wektor) {
+
+void zad_1(std::vector <int> & wektor) {
 	int n = 0;
 	std::cout << "Podaj ile elementow ma miec wektor: ";
 	std::cin >> n;
 	for (int i = 0; i < n; i++) {
 		wektor.push_back(rand());
 	}
-	return wektor;
 }
 
 void zad_2(std::vector <int> wektor) {
@@ -35,9 +38,8 @@ void zad_3(std::vector <int> wektor) {
 	std::cout << "Najwieksza liczba: " << max << std::endl;
 }
 
-std::vector <int> zad_4(std::vector <int> wektor) {
+void zad_4(std::vector <int> & wektor) {
 	std::sort(wektor.begin(), wektor.end());
-	return wektor;
 }
 
 struct zad5_punkt {
@@ -50,14 +52,47 @@ struct zad5_kolo {
 	float r;
 };
 
+void zad_5_a(std::vector <zad5_kolo> & kola) {
+	int n = 0;
+	std::cout << "Podaj ile kol ma miec wektor: ";
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		zad5_kolo kolo = { {rand(), rand()}, rand() };
+		kola.push_back(kolo);
+	}
+}
 
+int zad_5_b(std::vector <zad5_kolo> kola) {
+	float min = 0;
+	int min_idx = 0;
+	for (int i = 0; i < kola.size(); i++) {
+		float pole = M_PI * pow(kola[i].r, 2);
+		std::cout << i << " " << pole << std::endl;
+		if (i == 0) min = pole;
+		else if (pole < min) {
+			min = pole;
+			min_idx = i;
+		}
+	}
+	return min_idx;
+}
+
+void zad_2x(std::vector <zad5_kolo> wektor) {
+	for (int i = 0; i < wektor.size(); i++) {
+		std::cout << " " << wektor[i].punkt.x << " " << wektor[i].punkt.y << " " << wektor[i].r << std::endl;
+	}
+}
 
 int main() {
 	srand(time(NULL));
-	std::vector <int> wektor;
-	wektor = zad_1(wektor);
-	zad_2(wektor);
-	zad_3(wektor);
-	wektor = zad_4(wektor);
-	zad_2(wektor);
+	//std::vector <int> wektor;
+	std::vector <zad5_kolo> kola;
+	//zad_1(wektor);
+	//zad_2(wektor);
+	//zad_3(wektor);
+	//zad_4(wektor);
+	//zad_2(wektor);
+	zad_5_a(kola);
+	zad_2x(kola);
+	std::cout << zad_5_b(kola);
 }
