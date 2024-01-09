b1cdcf55b1e61c577e9913e09c69cce7e1bf158b
Kamil Korzeniewski <90689600+Korzeniewsky@users.noreply.github.com>
2024-01-09 09:43:51 +0100

    lab9
    lab9 + pliki te(k)stowe do tego

---
+++ lab9.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <ctime>
+
+
+int main() {
+    srand(time(NULL));
+
+    std::ifstream file;
+    std::string word;
+    std::map <std::string, int> slowa;
+    int liczba = 0, rand_liczba = 0;
+
+    file.open("beemovie.txt");
+
+    // Zliczanie słów
+    if (file) {
+        while (file >> word)
+        {
+            liczba++;
+            slowa[word]++;
+        }
+        file.close();
+    }
+
+    // Wypisanie słów i ich częstotliwości
+    for (const auto p : slowa)
+    {
+        std::cout << p.first << " " << p.second << std::endl;
+    }
+
+    // Generowanie zdań z losowych słów
+    for (int i = 0; i < 10; i++) {
+        int x = 0;
+        rand_liczba = 1 + (rand() % liczba);
+        for (const auto p : slowa)
+        {
+            x += p.second;
+            if (x >= rand_liczba) {
+                std::cout << p.first << " ";
+                break;
+            }
+        }
+    }
+    
+
+}
+
